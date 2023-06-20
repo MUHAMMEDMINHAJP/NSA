a77d287522cf63e6f05fd1151c854f1b5178ae63
muhammed minhaj p <116252656+MUHAMMEDMINHAJP@users.noreply.github.com>
2023-06-20 12:54:27 +0530

    Add files via upload

---
+++ 5/compare.sh
@@ -0,0 +1,19 @@
+# 5. Shell script to compare two files. If the files are identical delete one
+
+if [ $# -ne 0 ]
+then
+echo "Syntax error"
+exit
+fi
+echo "Enter a file name : "
+read a
+echo "Enter another file name : "
+read b
+cmp $a $b
+if [ $? -eq 0 ]
+then
+rm $b
+echo "Removed duplicate file"
+else
+echo "Duplicate file not exists"
+fi
